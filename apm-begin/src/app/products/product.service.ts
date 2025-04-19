b80db260b79e9d25bcc22fa2ac38fe4042f355af
Viktor Tishchenko <9043258451vvt@gmail.com>
2025-04-19 21:04:09 +0600

    feat: add HOC w reviews

--- apm-begin/src/app/products/product.service.ts
+++ apm-begin/src/app/products/product.service.ts
@@ -1,8 +1,18 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { catchError, Observable, tap, throwError } from 'rxjs';
+import {
+  catchError,
+  map,
+  Observable,
+  of,
+  switchMap,
+  tap,
+  throwError,
+} from 'rxjs';
 import { Product } from './product';
 import { HttpErrorService } from '../utilities/http-error.service';
+import { ReviewService } from '../reviews/review.service';
+import { Review } from '../reviews/review';
 
 @Injectable({
   providedIn: 'root',
@@ -11,6 +21,7 @@ export class ProductService {
   private productsUrl = 'api/products';
   private http = inject(HttpClient);
   private errorService = inject(HttpErrorService);
+  private reviewsService = inject(ReviewService);
 
   getProducts(): Observable<Product[]> {
     return this.http.get<Product[]>(this.productsUrl).pipe(
@@ -24,11 +35,20 @@ export class ProductService {
   getProduct(id: number): Observable<Product> {
     const productUrl = this.productsUrl + '/' + id;
     return this.http.get<Product>(productUrl).pipe(
-      tap(() => {
-        console.log('halo single product');
-      }),
+      switchMap((product) => this.getProductWithReviews(product)),
+      tap((x) => console.log('halo single product', x)),
       catchError((err) => this.handleError(err))
     );
+  }
+
+  getProductWithReviews(product: Product): Observable<Product> {
+    if (product.hasReviews) {
+      return this.http
+        .get<Review[]>(this.reviewsService.getReviewUrl(product.id))
+        .pipe(map((reviews) => ({ ...product, reviews } as Product)));
+    } else {
+      return of(product);
+    }
   }
 
   handleError(err: HttpErrorResponse): Observable<never> {
