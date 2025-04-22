cac0d8b34f4d382d4844a77f3cf4d89bf2998b2c
Viktor Tishchenko <9043258451vvt@gmail.com>
2025-04-22 16:23:36 +0600

    feat: sgnl.calculations

--- apm-begin/src/app/cart/cart.service.ts
+++ apm-begin/src/app/cart/cart.service.ts
@@ -12,6 +12,21 @@ export class CartService {
     this.cartItems().reduce((qutAcc, item) => qutAcc + item.quantity, 0)
   );
 
+  subTotal = computed(() =>
+    this.cartItems().reduce(
+      (accTotal, item) => accTotal + item.quantity * item.product.price,
+      0
+    )
+  );
+
+  deliveryFee = computed<number>(() => (this.subTotal() < 50 ? 5.99 : 0));
+
+  tax = computed(() => Math.round(this.subTotal() * 10.75) / 100);
+
+  totalPrice = computed(
+    () => this.subTotal() + this.deliveryFee() + this.tax()
+  );
+
   eLength = effect(() =>
     console.log('this.cartItems.length :>> ', this.cartItems().length)
   );
